a5dcb09cda4402b285c6bcc61cae16cc4ec18257
RR <RubenR42@icloud.com>
2020-03-21 22:14:55 -0700

    adds edit and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('marmots/', views.marmots_index, name='index'),
-    path('marmots/<int:marmot_id>/', views.marmots_index, name='detail'),
+    path('marmots/<int:marmot_id>/', views.marmots_detail, name='detail'),
+    path('marmots/create/', views.MarmotCreate.as_view(), name='marmots_create'),
+    path('marmots/<int:pk>/update/', views.MarmotUpdate.as_view(), name='marmots_update'),
+    path('marmots/<int:pk>/delete/', views.MarmotDelete.as_view(), name='marmots_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Marmot
 
-from django.http import HttpResponse
+class MarmotCreate(CreateView):
+  model = Marmot
+  fields = '__all__'
 
+class MarmotUpdate(UpdateView):
+  model = Marmot
+  fields = ['species', 'description', 'age']
 
-# Create your views here.
+class MarmotDelete(DeleteView):
+  model = Marmot
+  success_url = '/marmots/'
+
 def home(request):
-    return HttpResponse ('<h1>Hello [\OmO/]</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def marmots_index(request):
+    marmots = Marmot.objects.all()
     return render(request, 'marmots/index.html', { 'marmots': marmots })
+
+def marmots_detail(request, marmot_id):
+  marmot = Marmot.objects.get(id=marmot_id)
+  return render(request, 'marmots/detail.html', { 'marmot': marmot })
